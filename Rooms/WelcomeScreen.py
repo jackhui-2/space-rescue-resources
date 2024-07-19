6807fa2ab82787a232bc2d28f1efa361d6477a39
DamoM73 <40050142+DamoM73@users.noreply.github.com>
2024-07-19 14:00:05 +1000

    finished gameplayroom

--- Rooms/WelcomeScreen.py
+++ Rooms/WelcomeScreen.py
@@ -1,4 +1,5 @@
 from GameFrame import Level
+from Objects.Title import Title
 
 class WelcomeScreen(Level):
     """
@@ -6,3 +7,9 @@ class WelcomeScreen(Level):
     """
     def __init__(self, screen, joysticks):
         Level.__init__(self, screen, joysticks)
+        
+        # set background image
+        self.set_background_image("Background.png")
+        
+        # add title object
+        self.add_room_object(Title(self, 240, 200))
